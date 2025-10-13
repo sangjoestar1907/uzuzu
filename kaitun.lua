a0ce3472aa593fa49e4e70ababd6d235d4902527
sangjoestar1907 <cheesesato1@gmail.com>
2025-10-13 10:54:31 +0700

    Update kaitun.lua

--- kaitun.lua
+++ kaitun.lua
@@ -1,4 +1,3 @@
-setfpscap(25)
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer 
 getgenv().Key = "f74d5aa69115bd072e705a90" 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))() 
